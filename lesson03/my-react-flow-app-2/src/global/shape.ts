a52cff74f907c932ca0735b32ff57a40e5701538
Duong <buiducduong1@gmail.com>
2024-11-06 19:59:06 +0700

    delete temp

--- lesson03/my-react-flow-app-2/src/global/shape.ts
+++
@@ -1,9 +0,0 @@
-export type NodeShape = 'square' | 'RoundedSquare' | 'Circle'
-
-export type EdgeShape = 'straight' | 'smoothstep' | 'bezier' | 'simple'
-export const edgeShapesValues = [
-	'straight',
-	'smoothstep',
-	'bezier',
-	'simple'
-] as const
